6514a52f164c2021016c8fc8e3a7f10c194f9497
cataYT <firzt.superb.pro7@gmail.com>
2024-04-14 14:49:18 +0530

    added crosspost_message

--- discord_functions.py
+++ discord_functions.py
@@ -10,26 +10,27 @@ HEADERS = {
     "authorization": BOT_TOKEN
 }
 
+BASE_URL = "https://discord.com/api/v9"
+
 def send_message(message, channelID=1098207196505968690):
-    url = f"https://discord.com/api/v9/channels/{channelID}/messages"
+    url = f"{BASE_URL}/channels/{channelID}/messages"
     body = {   
         "content": message,
-        "tts": False, # text to speech
     }
     result = requests.post(url, json=body, headers=HEADERS).content.decode("utf-8")
     return json.loads(result)
 
 def delete_message(messageID, channelID=1098207196505968690):
-    url = f"https://discord.com/api/v9/channels/{channelID}/messages/{messageID}"
+    url = f"{BASE_URL}/channels/{channelID}/messages/{messageID}"
     return requests.delete(url=url, headers=HEADERS)
 
 def get_user_info(userID=925772708883615754, serverID=1119538529450594314, auth=BOT_TOKEN): # bot cannot do this
-    url = f"https://discord.com/api/v9/users/{userID}/profile?with_mutual_guilds=true&with_mutual_friends=false&with_mutual_friends_count=false&guild_id={serverID}"
+    url = f"{BASE_URL}/users/{userID}/profile?with_mutual_guilds=true&with_mutual_friends=false&with_mutual_friends_count=false&guild_id={serverID}"
     result = requests.get(url=url, headers=HEADERS).content.decode("utf-8")
     return json.loads(result)
 
 def login(token=BOT_TOKEN):
-    url = 'https://discord.com/api/v9/auth/login'
+    url = f'{BASE_URL}/v9/auth/login'
     headers = {
         'Authorization': token
     }
@@ -44,7 +45,7 @@ def login(token=BOT_TOKEN):
         return None
     
 def create_channel(name, serverID, parentID=None):
-    url = f"https://discord.com/api/v9/guilds/{serverID}/channels"
+    url = f"{BASE_URL}/guilds/{serverID}/channels"
     headers = {
         "authorization": BOT_TOKEN,
         "content-type": "application/json"
@@ -59,12 +60,12 @@ def create_channel(name, serverID, parentID=None):
     return json.loads(result)
 
 def delete_channel(channelID):
-    url = f"https://discord.com/api/v9/channels/{channelID}"
+    url = f"{BASE_URL}/channels/{channelID}"
     result = requests.delete(url=url, headers=HEADERS).content.decode("utf-8")
     return json.loads(result)
 
 def update_roles(roles, userID, serverID):
-    url = f"https://discord.com/api/v9/guilds/{serverID}/members/{userID}"
+    url = f"{BASE_URL}/guilds/{serverID}/members/{userID}"
     body = {
         "roles": roles
     }
@@ -72,7 +73,7 @@ def update_roles(roles, userID, serverID):
     return json.loads(result)
 
 def get_members_info(serverID, limit=250):
-    url = f"https://discord.com/api/v9/guilds/{serverID}/members-search"
+    url = f"{BASE_URL}/guilds/{serverID}/members-search"
     body = {
         "or_query": {},
         "and_query": {},
@@ -82,17 +83,25 @@ def get_members_info(serverID, limit=250):
     return json.loads(result)
 
 def pin_message(channelID, messageID):
-    url = f"https://discord.com/api/v9/channels/{channelID}/pins/{messageID}"
+    url = f"{BASE_URL}/channels/{channelID}/pins/{messageID}"
     result = requests.put(url=url, headers=HEADERS).content.decode("utf-8")
     return json.loads(result)
 
 def unpin_message(channelID, messageID):
-    url = f"https://discord.com/api/v9/channels/{channelID}/pins/{messageID}"
+    url = f"{BASE_URL}/channels/{channelID}/pins/{messageID}"
     result = requests.delete(url=url, headers=HEADERS).content.decode("utf-8")
     return json.loads(result)
 
 def get_pins(channelID):
-    url = f"https://discord.com/api/v9/channels/{channelID}/pins"
+    url = f"{BASE_URL}/channels/{channelID}/pins"
     result = requests.get(url=url, headers=HEADERS).content.decode("utf-8")
     return json.loads(result)
-    
+
+def crosspost_message(channel_id, message_id):
+    url = f'https://discord.com/api/v9/channels/{channel_id}/messages/{message_id}/crosspost'
+    headers = {
+        'Authorization': BOT_TOKEN,
+        'Content-Type': 'application/json',
+    }
+    result = requests.post(url, headers=headers).content.decode("utf-8")
+    return json.loads(result)
